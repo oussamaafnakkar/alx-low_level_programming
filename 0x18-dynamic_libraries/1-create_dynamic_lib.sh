a1e7659cb000afcbaa7a72834baa14a95e2f6010
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2023-08-21 15:41:48 +0000

    Update 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-gcc -fPIC -c *.c
+
+# Compile the .c files into a shared dynamic library
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fPIC -c *.c
 gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm -f *.o
